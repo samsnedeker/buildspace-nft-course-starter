15b4cb5a5b88108b8d23d74b84cd9acbfb7c4194
Sam Snedeker <snedekersa@gmail.com>
2022-03-04 13:54:27 -0500

    change twitter handle

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import twitterLogo from './assets/twitter-logo.svg';
 import React from "react";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'snedsas';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
@@ -33,7 +33,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`from @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
