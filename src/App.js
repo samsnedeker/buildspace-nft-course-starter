828e604481133dd754c8a6d4fd416c3362abd75d
Sam Snedeker <snedekersa@gmail.com>
2022-03-04 14:03:40 -0500

    change font

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">Sam's First NFT Collection.</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            Thanks to @_buildspace.
           </p>
           {renderNotConnectedContainer()}
         </div>
