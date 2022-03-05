e4537a810d0e94bcad9fd3671e45aace02289f44
Sam Snedeker <snedekersa@gmail.com>
2022-03-05 10:07:06 -0500

    event listener

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ethers, providers } from 'ethers';
 import myEpicNft from './utils/MyEpicNFT.json';
 
 // Constants
+const CONTRACT_ADDRESS = '0x857E54251c9e7A6cC19fd26CbCdf15C5f5979F4c';
 const TWITTER_HANDLE = 'snedsas';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
@@ -30,11 +31,34 @@ const App = () => {
       const account =accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+      setupEventListener();
     } else {
       console.log("No authorized account found.");
     }
   }
 
+  const setupEventListener = async() => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+        });
+        console.log("Setup event listener!");
+      } else {
+        console.log("Etheruem object doesn't exist!");
+      }
+    } catch(error) {
+      console.log(error);
+    }
+  }
+ 
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -55,8 +79,6 @@ const App = () => {
   }
 
   const askContractToMintNFT = async() => {
-    const CONTRACT_ADDRESS = '0x8Ba66180d9B285B9C28989A1C7997B0C04Ff0a5C';
-
     try {
       const { ethereum } = window;
 
